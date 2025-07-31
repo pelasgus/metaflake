b4d2e2bd48e1cb0ea6371f00cb8fdebd0d419f6c
D.A.Pelasgus <demetrius@pelasgus.xyz>
2025-07-31 05:16:13 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,42 @@
 {
   description = "A metaflake to handle inputs";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    impermanence.url = "github:nix-community/impermanence";
-    age.url = "github:ryantm/agenix";
-    age.inputs.nixpkgs.follows = "nixpkgs";
-    sops.url = "github:Mic92/sops-nix";
-    sops.inputs.nixpkgs.follows = "nixpkgs";
-    nixarr.url = "github:rasmus-kirk/nixarr";
-    nixarr.inputs.nixpkgs.follows = "nixpkgs";
-    leanback.url = "github:Jovian-Experiments/Jovian-NixOS";
-    leanback.inputs.nixpkgs.follows = "nixpkgs";
-    partitioner.url = "github:nix-community/disko";
-    partitioner.inputs.nixpkgs.follows = "nixpkgs";
-    hardware.url = "github:NixOS/nixos-hardware";
-    customiser.url = "github:danth/stylix";
-    customiser.inputs.nixpkgs.follows = "nixpkgs";
-    boot-themes.url = "github:vinceliuice/grub2-themes";
-    boot-themes.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
-    browser-extensions.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-    browser-extensions.inputs.nixpkgs.follows = "nixpkgs";
-    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-    apple-fonts.inputs.nixpkgs.follows = "nixpkgs";
-    apple-emoji.url = "github:typedrat/apple-emoji-linux?ref=fix-flake-on-unstable";
-    apple-emoji.inputs.nixpkgs.follows = "nixpkgs";
-    operator-mono.url = "github:pelasgus/operator-mono";
-    operator-mono.inputs.nixpkgs.follows = "nixpkgs";
-    formatter.url = "github:kamadorueda/alejandra/4.0.0";
-    formatter.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.import-tree.url = "github:vic/import-tree";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.impermanence.url = "github:nix-community/impermanence";
+  inputs.age.url = "github:ryantm/agenix";
+  inputs.age.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.sops.url = "github:Mic92/sops-nix";
+  inputs.sops.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixarr.url = "github:rasmus-kirk/nixarr";
+  inputs.nixarr.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.leanback.url = "github:Jovian-Experiments/Jovian-NixOS";
+  inputs.leanback.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.partitioner.url = "github:nix-community/disko";
+  inputs.partitioner.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.hardware.url = "github:NixOS/nixos-hardware";
+  inputs.customiser.url = "github:danth/stylix";
+  inputs.customiser.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.boot-themes.url = "github:vinceliuice/grub2-themes";
+  inputs.boot-themes.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.zen-browser.url = "github:MarceColl/zen-browser-flake";
+  inputs.zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.browser-extensions.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+  inputs.browser-extensions.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
+  inputs.apple-fonts.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.apple-emoji.url = "github:typedrat/apple-emoji-linux?ref=fix-flake-on-unstable";
+  inputs.apple-emoji.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.operator-mono.url = "github:pelasgus/operator-mono";
+  inputs.operator-mono.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.formatter.url = "github:kamadorueda/alejandra/4.0.0";
+  inputs.formatter.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inputs@{ self, ... }: {
-    # Re-export inputs so other flakes can use them
-    _module.args = {
-      metaflake-inputs = inputs;
-    };
+    # expose all inputs under a clean namespace
+    _module.args.metaflake-inputs = inputs;
   };
 }
